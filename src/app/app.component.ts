7c2c2a69f90f20ff7daed2f82388709da2419a20
Pratap Mishra <pkmishra.2391@gmail.com>
2024-01-10 00:44:09 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { UserloginComponent } from './userlogin/userlogin.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,UserloginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'global-bank';
+  title = 'Global Bank';
 }
